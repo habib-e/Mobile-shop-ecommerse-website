8c77d7deb94f0e2619eb1b3e88c6c3d0d1759564
habib-e <basharmd978@gmail.com>
2021-10-26 21:32:29 +0600

    successfully converted HTML to PHP

---
+++ cart.php
@@ -0,0 +1,22 @@
+<?php
+    //include header.php file
+    include('header.php');
+?>
+
+<?php
+
+    /* include cart-template */
+    include('Template/_cart-template.php');
+    /* include cart-template */
+
+    /* include top sale */
+    include('Template/_new-phones.php');
+    /* include top sale */
+
+?>
+
+
+<?php
+    //include footer.php file
+    include('footer.php');
+?>
---
+++ header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--abs-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mobile Shopee</title>
+
+    <!--Bootstrap CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!--Owl-carousel CDN-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+          integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+          integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!--font awesome icons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+          integrity="sha512-L7MWcK7FNPcwNqnLdZq86lTHYLdQqZaz5YcAgE+5cnGmlw8JT03QB2+oxL100UeB6RlzZLUxCGSS4/++mNZdxw=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+    <!--Custom CSS file-->
+    <link rel="stylesheet" href="style.css">
+
+
+    <!--Slide photo test section of head-->
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+
+    <!--!Slide photo test section of head-->
+
+
+</head>
+
+<body>
+
+<!--start #header test for git push-->
+<header id="header">
+    <div class="strip d-flex justify-content-between px-4 py-1 bg-light">
+        <p class="font-rale font-size-13 text-dark m-0">Bangladesh University of Business & Technology Address: Plot #
+            77-78, Road # 9, Rupnagar R/A Mirpur-2 Dhaka, Dhaka 1216</p>
+        <div class="font-rale font-size-14">
+            <a href="#" class="px-3 border-right border-left text-dark">Login</a>
+            <a href="#" class="px-3 border-right text-dark">Whishlist (0)</a>
+        </div>
+    </div>
+
+    <!--Primary Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <!--Error here if we do it like him-->
+        <a class="navbar-brand" href="#">Smartphone Ecommerce website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav m-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">On Sale</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Category</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products <i class="fas fa-chevron-down"></i> </a>
+                    <!--down symbol-->
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Category <i class="fas fa-chevron-down"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Coming Soon</a>
+                </li>
+            </ul>
+            <form action="#" class="font-size-14 font-rale">
+                <!--same problem-->
+                <a href="#" class="py-2 rounded-pill color-dark">
+                    <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i> </span>
+                    <span class="px-3 py-2 rounded-pill text-dark bg-light">0</span>
+                </a>
+
+            </form>
+        </div>
+    </nav>
+    <!--!Primary Navigation-->
+
+</header>
+<!--!start #header-->
+<!--start #main-site-->
+<main id="main-site">
