b0bef7af39cf22c1a8add7609cd129094097ffda
habib-e <basharmd978@gmail.com>
2021-10-27 17:15:51 +0600

    Updating Blogs

--- Template/_new-phones.php
+++ Template/_new-phones.php
@@ -1,143 +1,34 @@
 <!--New Phone-->
+<?php
+    shuffle($product_shuffle);
+?>
 <section id="new-phones">
     <div class="container">
         <h4 class="font-rubik font-size-20">New Phones</h4>
         <hr>
         <!--Owl carousel-->
         <div class="owl-carousel owl-theme">
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Samsung Galaxy S10</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
+            <?php foreach ($product_shuffle as $item){ ?>
+                <div class="item py-2 bg-light">
+                    <div class="product font-rale">
+                        <a href="#"><img src="<?php echo $item['item_image'] ?? "./assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
+                        <div class="text-center">
+                            <h6><?php echo $item['item_name'] ?? "Unknown"; ?></h6>
+                            <div class="rating text-warning font-size-12">
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="fas fa-star"></i></span>
+                                <span><i class="far fa-star"></i></span>
+                            </div>
+                            <div class="price py-2">
+                                <span><?php echo $item['item_price'] ?? "0"; ?></span>
+                            </div>
+                            <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
                         </div>
-                        <div class="price py-2">
-                            <span>৳53,300</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
                     </div>
                 </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/2.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Redmi Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳22,990</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/3.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Redmi Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳16,500</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Samsung Galaxy 10</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳53,300</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/2.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Redmi Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳22,990</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/1.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Samsung Galaxy 10</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳53,300</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div class="item py-2 bg-light">
-                <div class="product font-rale">
-                    <a href="#"><img src="./assets/products/2.png" alt="product1" class="img-fluid"></a>
-                    <div class="text-center">
-                        <h6>Redmi Note 7</h6>
-                        <div class="rating text-warning font-size-12">
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="fas fa-star"></i></span>
-                            <span><i class="far fa-star"></i></span>
-                        </div>
-                        <div class="price py-2">
-                            <span>৳16,500</span>
-                        </div>
-                        <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
-                    </div>
-                </div>
-            </div>
+            <?php }//closing foreach function?>
         </div>
         <!--!Owl carousel-->
     </div>
