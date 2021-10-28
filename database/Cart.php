220daa99ff08569dbadf2288e6cd8020b6e41c87
habib-e <basharmd978@gmail.com>
2021-10-29 00:35:11 +0600

    Get rid of duplicate item on cart

--- database/Cart.php
+++ database/Cart.php
@@ -69,4 +69,17 @@ class Cart
             return sprintf('%.2f' , $sum);
         }
     }
+
+    // get item_it of shopping cart list
+    public function getCartId($cartArray = null, $key = "item_id"){
+        if ($cartArray != null){
+            $cart_id = array_map(function ($value) use($key){
+                return $value[$key];
+            }, $cartArray);
+            return $cart_id;
+        }
+    }
+
+
+
 }
